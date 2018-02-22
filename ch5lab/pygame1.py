8597c96d766ce6e70385267562676803d6f55f97
Jake <jake.townsend@pulteney.sa.edu.au>
2018-02-23 09:47:59 +1030

    lab5

--- ch5lab/pygame1.py
+++ ch5lab/pygame1.py
@@ -1,21 +1,11 @@
-"""
- Simple graphics demo
 
- Sample Python/Pygame Programs
- Simpson College Computer Science
- http://programarcadegames.com/
- http://simpson.edu/computer-science/
-
-"""
-
-# Import a library of functions called 'pygame'
 import pygame
 import random
 
-# Initialize the game engine
+
 pygame.init()
 
-# Define some colors
+
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
@@ -28,22 +18,19 @@ GREEN2 = (34,139,34)
 PI = 3.141592653
 YOff = 100
 
-# Set the height and width of the screen
 size = (400, 500)
 screen = pygame.display.set_mode(size)
 
 pygame.display.set_caption("Professor Craven's Cool Game")
 
-# Loop until the user clicks the close button.
 done = False
 clock = pygame.time.Clock()
 
-# Loop as long as done == False
 while not done:
 
-    for event in pygame.event.get():  # User did something
-        if event.type == pygame.QUIT:  # If user clicked close
-            done = True  # Flag that we are done so we exit this loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+            done = True  
         elif event.type == pygame.MOUSEBUTTONDOWN:
             print("User pressed a mouse button")
             print(pygame.mouse.get_pos()
